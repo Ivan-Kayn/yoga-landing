2a5482a2ff6e8e4985029794a4e3c03823c9e35f
Ivan <ionel.cainareanu1@gmail.com>
2019-12-27 09:46:32 +0100

    cost calculator working

--- js/script.js
+++ js/script.js
@@ -232,5 +232,51 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    //Calculator
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener('change', function () {
+        personsSum = +this.value;
+        if (personsSum != 0 || personsSum != '') {
+            total = (daysSum + personsSum) * 4000;
+        }
+
+        if (restDays.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    restDays.addEventListener('change', function () {
+        daysSum = +this.value;
+        if (daysSum != 0 || daysSum != '') {
+            total = (daysSum + personsSum) * 4000;
+        }
+
+        if (persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function () {
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    })
 
 });
