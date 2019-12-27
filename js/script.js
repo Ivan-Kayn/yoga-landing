ac727c33a130f18e87b1a84db48741b2215f1be7
Ivan <ionel.cainareanu1@gmail.com>
2019-12-27 09:17:02 +0100

    working slider

--- js/script.js
+++ js/script.js
@@ -141,7 +141,7 @@ window.addEventListener('DOMContentLoaded', function () {
         elem.addEventListener('submit', function (event) {
             event.preventDefault();
             elem.appendChild(statusMessage);
-            
+
             let request = new XMLHttpRequest();
             request.open('POST', 'server.php');
             request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
@@ -173,9 +173,64 @@ window.addEventListener('DOMContentLoaded', function () {
         });
     };
     sendForm(form);
+
     // contact form
+
     let contactForm = document.getElementById('form'),
         contactInput = contactForm.getElementsByTagName('input');
     sendForm(contactForm);
+
+    //Slider 
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function () {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function () {
+        plusSlides(+1);
+    });
+
+    dotsWrap.addEventListener('click', function (event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                currentSlide(i);
+            }
+        }
+    });
+
 
 });
